494857c59081e32ad889cee8ecc0706338acdf78
LucasAli18 <104177558+LucasAli18@users.noreply.github.com>
2023-08-04 19:11:03 -0300

    Subir Website
    He subido todos mis archivos

--- js/questions.js
+++ js/questions.js
@@ -1,18 +1,16 @@
 (function(){
     const titleQuestions = [...document.querySelectorAll('.questions__title')];
-    console.log(titleQuestions)
-
     titleQuestions.forEach(question =>{
         question.addEventListener('click', ()=>{
             let height = 0;
-            let answer = question.nextElementSibling;
-            let addPadding = question.parentElement.parentElement;
+            let answer = question.nextElementSibling; /* Selecciona el elemento que sigue que en este caso es P */
+            let addPadding = question.parentElement.parentElement; /* Selecciona el padre del elemento */
 
             addPadding.classList.toggle('questions__padding--add');
-            question.children[0].classList.toggle('questions__arrow--rotate');
+            question.children[0].classList.toggle('questions__arrow--rotate'); /* Obtiene la clase del hijo y Agrega o elminina la clase colocada en el parentesis dependiendo si existe o no la clase */
 
-            if(answer.clientHeight === 0){
-                height = answer.scrollHeight;
+            if(answer.clientHeight === 0){ /* Pregunto si tiene un height igual a cero */
+                height = answer.scrollHeight; /*  Nos da el alto minimo para que se muestre un elemento */
             }
 
             answer.style.height = `${height}px`;
